fc4d4e7eff9c3b4a9265a74218e8f568acf02d0d
Chris Peters <chrispeters@redhat.com>
2020-12-08 17:14:35 -0500

    Fix import path

--- src/main/java/com/example/demo/UserRepository.java
+++ src/main/java/com/example/demo/UserRepository.java
@@ -2,7 +2,7 @@ package com.example.demo;
 
 import org.springframework.data.repository.CrudRepository;
 
-import com.example.accessingdatamysql.User;
+import com.example.demo.User;
 
 // This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
 // CRUD refers Create, Read, Update, Delete
